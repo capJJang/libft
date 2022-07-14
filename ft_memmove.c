2684ef3f1f29580e67d9e468c98bfe2b4f70e28b
Segun An <segan@c3r4s6.42seoul.kr>
2022-07-14 14:20:10 +0900

    first commit

---
+++ ft_memmove.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: segan <segan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 23:40:45 by segan             #+#    #+#             */
+/*   Updated: 2022/07/14 13:52:23 by segan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	temp;
+	unsigned char	*src_ptr;
+	unsigned char	*dst_ptr;
+
+	src_ptr = (unsigned char *)src;
+	dst_ptr = dst;
+	while (len--)
+	{
+		temp = *src_ptr;
+		*dst_ptr = temp;
+		*src_ptr = temp;
+	}
+	return (dst);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	char str1[] = "Hello world!";
+	char str2[30];
+	char str3[30];
+	ft_memmove(str2, str1, 3);
+	printf("my func : %s\n", str2);
+	ft_bzero(str2, 30);
+	memmove(str3, str1, 3);
+	printf("lib func : %s", str3);
+}
---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: segan <segan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 17:08:31 by segan             #+#    #+#             */
+/*   Updated: 2022/07/14 12:01:20 by segan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
