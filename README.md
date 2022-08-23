6646a9524c1c022ca7a533c1ecde7d0c58dc2177
segan <segan@DESKTOP-K4OE2R1.localdomain>
2022-08-23 23:29:21 +0900

    ,,,,

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ What I've learned
 
  - size of register is kind of cpu architecture.
    like x86 or x64
+
+- tail call is efficient for stack memory. because is does not store return address at stack!
+
+- dangling pointer, must assign null after free. must initialize node
--- ft_split.c
+++ ft_split.c
@@ -12,25 +12,23 @@
 
 #include "libft.h"
 
-int	total_word_length(char const *s, int c)
+size_t	count_word(char const *s, int c)
 {
-	size_t	len;
+	size_t	cnt;
 
-	len = 0;
+	cnt = 0;
 	while (*s)
 	{
-		if (*s == c && len == 0) // 구분자도 같이 포함함?
+		if (*s != c)
 		{
-			s++;
-			continue ;
-		}
-		else
-		{
-				len++;
+			cnt++;
+			while (*s != c && *s != 0)
 				s++;
 		}
+		else
+			s++;
 	}
-	return (len);
+	return (cnt);
 }
 
 char	**free_all(char **s)
@@ -41,6 +39,7 @@ char	**free_all(char **s)
 	while (s[i])
 		free(s[i++]);
 	free(s);
+	s = NULL;
 	return ((void *)0);
 }
 
@@ -50,29 +49,31 @@ char	**ft_split(char const *s, char c)
 	size_t	i;
 	char	**str;
 
-	str = (char **)malloc(total_word_length(s, c));
+	str = (char **)malloc(count_word(s, c) + 1);
 	i = 0;
 	while (*s)
 	{
-		temp_length = 0;
-		while (*s != c && *s != 0)
+		if (*s != c)
 		{
-			temp_length++;
-			s++;
+			temp_length = 0;
+			while (*s != c && *s != 0)
+			{
+				temp_length++;
+				s++;
+			}
+			str[i] = (char *)malloc(temp_length + 1);
+			if (str[i] == (void *)0)
+				return (free_all(str));
+			ft_strlcpy(str[i], s - temp_length, temp_length + 1);
+			i++;
 		}
-		str[i] = (char *)malloc(temp_length + 1);
-		if (str[i] == (void *)0)
-			return (free_all(str));
-		ft_strlcpy(str[i], s - temp_length, temp_length + 1);
-		i++;
-		if (*s == 0)
-			break ;
-		s++;
+		else
+			s++;
 	}
+	str[i] = 0;
 	return (str);
 }
 
-/*
 #include <unistd.h>
 
 void	ft_print_result(char const *s)
@@ -85,17 +86,80 @@ void	ft_print_result(char const *s)
 	write(1, s, len);
 }
 
-#include <stdio.h>
 
-int main(void)
+int		main()
 {
-	char **tabstr;
+	char	**tabstr;
+	int		i;
 
-	tabstr = ft_split("lorem ipsum dolor", ' ');
-	for (int i = 0; i < 3; i++ )
-	{
-		ft_print_result(tabstr[i]);
-		write(1, "\n", 1);
-	}
+	alarm(5);
+
+	i = 0;
+		// if (!(tabstr = ft_split("          ", ' ')))
+		// 	ft_print_result("NULL");
+		// else
+		// {
+		// 	while (tabstr[i] != NULL)
+		// 	{
+		// 		ft_print_result(tabstr[i]);
+		// 		write(1, "\n", 1);
+		// 		i++;
+		// 	}
+		// }
+
+		if (!(tabstr = ft_split("lorem ipsum", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	
+		// if (!(tabstr = ft_split("   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ')))
+		// 	ft_print_result("NULL");
+		// else
+		// {
+		// 	while (tabstr[i] != NULL)
+		// 	{
+		// 		ft_print_result(tabstr[i]);
+		// 		write(1, "\n", 1);
+		// 		i++;
+		// 	}
+		// }
+
+		// if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i')))
+		// 	ft_print_result("NULL");
+		// else
+		// {
+		// 	while (tabstr[i] != NULL)
+		// 	{
+		// 		ft_print_result(tabstr[i]);
+		// 		write(1, "\n", 1);
+		// 		i++;
+		// 	}
+		// }
+
+		// if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'z')))
+		// 	ft_print_result("NULL");
+		// else
+		// {
+		// 	while (tabstr[i] != NULL)
+		// 	{
+		// 		ft_print_result(tabstr[i]);
+		// 		write(1, "\n", 1);
+		// 		i++;
+		// 	}
+		// }
+
+	
+		// if (!(tabstr = ft_split("", 'z')))
+		// 	ft_print_result("NULL");
+		// else
+		// 	if (!tabstr[0])
+		// 		ft_print_result("ok\n");
+	return (0);
 }
-*/
