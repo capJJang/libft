2a7a650ca404004d3c1a3e604b35b015d71f8044
Segun An <segan@c4r9s1.42seoul.kr>
2022-08-29 17:00:12 +0900

    end

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -17,7 +17,7 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
 	{
-		write(1, "-", 1);
+		ft_putchar_fd('-', fd);
 		if (n <= -10)
 			ft_putnbr_fd(n / 10 * -1, fd);
 		ft_putchar_fd(n % 10 * -1 + '0', fd);
