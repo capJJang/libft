9e3b5022fdd22069e8f672d54e762029398f8811
haha <dkstprjs@naver.com>
2022-09-03 02:33:20 +0900

    debugging : assign NULL after free

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -29,8 +29,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		ft_lstadd_back(&new_lst, temp);
 		lst = lst->next;
 	}
-	temp = NULL;
 	free(temp);
+	temp = NULL;
 	return (new_lst);
 }
 
